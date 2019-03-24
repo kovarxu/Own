99adc905b7242da153328ca30780494f7b3e6768
chao xu <979234374@qq.com>
2019-03-24 08:40:47 +0800

    add some react programs

---
+++ Ts/first/server.ts
@@ -0,0 +1,8 @@
+export class server {
+
+}
+
+export interface DataStore {
+  timestamp: number;
+  data: string;
+}
--- myreact/mytodo/src/index.js
+++ myreact/mytodo/src/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const EL = function (props) {
+//   return (
+//     <div>
+//       {props.name}
+//     </div>
+//   )
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// class EL extends React.Component {
+//   constructor() {
+//     this.name = 'name';
+//   }
+//   render () {
+//     return (
+//       <div>
+//         {this.props.name}
+//       </div>
+//     )
+//   }
+// }
+
+// function App () {
+//   return (
+//     <div>
+//       <EL name="sora"></EL>
+//       <EL name="lili"></EL>
+//       <EL name="jone"></EL>
+//     </div>
+//   )
+// }
+
+// function Clock (props) {
+//   return (
+//     <div>{props.date}</div>
+//   )
+// }
+
+class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString(),
+    }
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount () {
+    this.timer = null;
+  }
+
+  tick () {
+    this.setState({
+      date: new Date().toLocaleTimeString(),
+    })
+  }
+
+  render () {
+    return (
+      <div>{this.state.date}</div>
+    )
+  }
+}
+
+function tick () {
+  return ReactDOM.render(<Clock />, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+// ReactDOM.render(<App />, document.getElementById('root'));
