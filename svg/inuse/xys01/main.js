4f7e2fbd569ab3f82f9d7500bf632a10a79abdac
CX-SteelArm <979234374@qq.com>
2019-08-21 09:50:55 +0800

    okk

---
+++ svg/inuse/xys01/main.js
@@ -0,0 +1,37 @@
+const svgDoms = document.getElementsByTagName('svg')
+const svgs = Array.prototype.slice.call(svgDoms)
+const idList = []
+
+function main () {
+  svgs.forEach((svg, index) => {
+    let id = svg.id
+    if (id) idList.push(id)
+    svg.setAttribute('width', '100%')
+    svg.setAttribute('height', '500')
+    if (index) {
+      svg.classList.add('hide')
+    }
+  })
+
+  const text = new MainText()
+  const gui = new dat.GUI()
+  gui.remember(text)
+
+  gui.add(text, 'pattern', idList).onChange(value => {
+    let ele = document.getElementById(value)
+    if (ele) {
+      svgs.forEach((svg) => {
+        if (svg.id !== value) svg.classList.add('hide')
+        else svg.classList.remove('hide')
+      })
+    }
+  })
+
+  document.body.classList.remove('hide')
+}
+
+const MainText = function () {
+  this.pattern = idList[0]
+}
+
+main()
