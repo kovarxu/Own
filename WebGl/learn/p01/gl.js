e929a35bc673403ca64d59e07ee39989169fdb12
CX-SteelArm <979234374@qq.com>
2019-09-02 19:54:50 +0800

    modify

--- WebGl/learn/p01/gl.js
+++ WebGl/learn/p01/gl.js
@@ -6,12 +6,7 @@ const programInfo = twgl.createProgramInfo(gl, [vt_shader, fm_shader]);
 twgl.setAttributePrefix("a_")
 
 let sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(gl, 10, 20, 20);
-let cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 20);
-let coneBufferInfo = flattenedPrimitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 10, 1, true, false);
-
 const sphereVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sphereBufferInfo);
-const cubeVAO   = twgl.createVAOFromBufferInfo(gl, programInfo, cubeBufferInfo);
-const coneVAO   = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfo);
 
 // uniforms that are diffenert from each object
 const sphereUniforms = {
@@ -19,19 +14,16 @@ const sphereUniforms = {
   u_mulColor: new Float32Array([0.8, 0.5, 0.5, 1])
 }
 
-const cubeUniforms = {
-  u_matrix: [],
-  u_mulColor: new Float32Array([0.5, 0.5, 0.8, 1])
-}
-
-const coneUniforms = {
-  u_matrix: [],
-  u_mulColor: new Float32Array([0.5, 0.8, 0.5, 1])
-}
-
 // animation global variables
 let then = 0, currot = 0
 const rotPerSecond = 50
+
+// sphere list
+const spheres = Array.from({length: 50}, () => {return {
+  x: Math.random() * 600 - 300,
+  y: Math.random() * 400 - 200,
+  color: new Float32Array([Math.random(), Math.random(), Math.random(), 1])
+}})
 
 function main () {
   // draw first screen
@@ -59,18 +51,16 @@ function render (now) {
 
   // bind the attribute/buffer we set
   gl.bindVertexArray(sphereVAO)
-  gl.bindVertexArray(cubeVAO)
-  gl.bindVertexArray(coneVAO)
   
   gl.useProgram(programInfo.program)
   
-  drawObjects(now, width / height) 
+  drawObjects(now, width, height) 
   
   // we call requestAnimationFrame again to keep on animating
   requestAnimationFrame(render)
 }
 
-function drawObjects (now, ratio) {
+function drawObjects (now, width, height) {
   
   // inverse matrix of camera matrix
   let caPosition = [0, 0, 60], center = [0, 0, 0], up = [0, 1, 0]
@@ -79,8 +69,10 @@ function drawObjects (now, ratio) {
   let caMatrix = m4.lookAt(m4unit(), caPosition, center, up)
 
   // perspective matrix
+  let ratio = width / height
   let pov = Math.PI / 2
-  let perspective = m4.perspective(m4unit(), pov, ratio, 1, 2000)
+  // let perspective = m4.perspective(m4unit(), pov, ratio, 1, 2000)
+  let ortho = m4.ortho(m4unit(), -width/2, width/2, -height/2, height/2, 200, -200)
 
   /* ----------- animation ----------- */
   if (!then) then = now
@@ -94,38 +86,21 @@ function drawObjects (now, ratio) {
   let rotationX = m4rotateX(-toRad(currot) / 2)
   let translation = m4translate(0, 0, 0)
 
-  // draw sphere
-  gl.bindVertexArray(sphereVAO);
-  translation = m4translate(50, 0, 0)
+  for (let i = 0; i < spheres.length; i++) {
+    let sphere = spheres[i]
 
-  twgl.setUniforms(programInfo, { 
-    u_matrix: m4mul(rotationY, rotationX, translation, caMatrix, perspective),
-    u_mulColor: sphereUniforms.u_mulColor
-  })
+    // draw sphere
+    gl.bindVertexArray(sphereVAO);
+    translation = m4translate(sphere.x, sphere.y, 0)
 
-  twgl.drawBufferInfo(gl, sphereBufferInfo)
+    twgl.setUniforms(programInfo, { 
+      // u_matrix: m4mul(rotationY, rotationX, translation, caMatrix, perspective),
+      u_matrix: m4mul(rotationY, rotationX, translation, caMatrix, ortho),
+      u_mulColor: sphere.color
+    })
 
-  // draw cube
-  gl.bindVertexArray(cubeVAO);
-  translation = m4translate(0, 0, 0)
-
-  twgl.setUniforms(programInfo, {
-    u_matrix: m4mul(rotationY, rotationX, translation, caMatrix, perspective),
-    u_mulColor: cubeUniforms.u_mulColor
-  })
-
-  twgl.drawBufferInfo(gl, cubeBufferInfo)
-
-  // draw cone 
-  gl.bindVertexArray(coneVAO);
-  translation = m4translate(-50, 0, 0)
-
-  twgl.setUniforms(programInfo, {
-    u_matrix: m4mul(rotationY, rotationX, translation, caMatrix, perspective),
-    u_mulColor: coneUniforms.u_mulColor
-  })
-
-  twgl.drawBufferInfo(gl, coneBufferInfo)
+    twgl.drawBufferInfo(gl, sphereBufferInfo)
+  }
 }
 
 function resizeCanvas () {
