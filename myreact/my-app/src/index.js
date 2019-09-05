2a0f632cfe9df5c24ec4aa3e517523b9ed0577cb
CX-SteelArm <979234374@qq.com>
2019-09-05 19:52:41 +0800

    learn

--- myreact/my-app/src/index.js
+++ myreact/my-app/src/index.js
@@ -5,11 +5,34 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <button className="square" onClick={(e) => this.props.onClick(e)}>
         {this.props.value}
       </button>
     );
   }
+}
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div>{ this.props.content || 'no content' }</div>
+    )
+  }
+}
+
+function WarningBanner(props) {
+  if(!props.clickNum) {
+    return null;
+  } else if (props.clickNum === 5) {
+    return (
+      <audio controls></audio>
+    )
+  }
+  return (
+    <div className="displayed-num">
+      Warning!
+    </div>
+  )
 }
 
 class Board extends React.Component {
@@ -18,24 +41,38 @@ class Board extends React.Component {
     this.state = {
       "squares": Array(9).fill(null),
       "isXNext": true,
+      "clickNum": 0,
+      "content": 'no one wins'
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    return <Square value={this.state.squares[i]} onClick={(e) => this.handleClick(i,e)} />;
   }
 
-  handleClick (i) {
+  renderNote() {
+    return <Note content={this.state.content} />
+  }
+
+  renderWarning() {
+    return <WarningBanner clickNum={this.state.clickNum} />
+  }
+
+  handleClick (i,e) {
     const tmp = this.state.squares.slice();
     tmp[i] = this.state.isXNext ? 'X' : 'O';
+    console.log('event', e)
 
     const winer = calculateWinner(this.state.squares);
     if (winer) {
+      this.setState({'content': "has winner"})
       console.log('winner')
     } else {
       this.state.isXNext = ! this.state.isXNext;
       this.setState({'squares': tmp});
     }
+    this.setState({clickNum: this.state.clickNum + 1})
   }
 
   render() {
@@ -65,6 +102,8 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        {this.renderNote()}
+        {this.renderWarning()}
       </div>
     );
   }
@@ -90,6 +129,41 @@ function calculateWinner(squares) {
   return null;
 }
 
+class Remark extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      title: '',
+      content: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  changeState(name, value) {
+    this.setState({[name]: value})
+  }
+
+  handleSubmit() {
+    console.log(this.state.title + '~~' + this.state.content)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          评论题目:
+          <input value={this.title} placeholder="请输入题目" onChange={(e) => this.changeState('title', e.target.value)}/>
+        </div>
+        <div>
+          评论内容：
+          <textarea value={this.content} placeholder="请输入评论内容" onChange={(e) => this.changeState('content', e.target.value)}/>
+        </div>
+        <input type="submit" value="提交" />
+      </form>
+    )
+  }
+}
+
 class Game extends React.Component {
   render() {
     return (
@@ -100,6 +174,10 @@ class Game extends React.Component {
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
+        </div>
+        <div></div>
+        <div>
+          <Remark />
         </div>
       </div>
     );
