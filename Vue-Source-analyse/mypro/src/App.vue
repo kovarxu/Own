f838f3eab359ec83314eadbf98a9c4ab1d722b77
chao xu <979234374@qq.com>
2019-10-06 23:50:41 +0800

    add some vue notes

--- Vue-Source-analyse/mypro/src/App.vue
+++ Vue-Source-analyse/mypro/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png" ref="logoImg">
-    <div>I am vue</div>
+    <div ref="foo">{{ bar }}</div>
     <div>{{ messageBox }}</div>
   </div>
 </template>
@@ -10,11 +10,34 @@
 export default {
   name: 'App',
   props: ['message-box'],
+  data () {
+    return {
+      bar: 'I am Vue'
+    }
+  },
+  watch: {
+    bar (newVal, oldVal) {
+      console.log(newVal + ' || The old version is ' + oldVal)
+    }
+  },
   mounted () {
     console.log(
       'component: app is mounted' + 
       'log img is ready: ', this.$refs.logoImg.complete
       )
+    let foo = this.$refs.foo
+    console.log(foo.innerHTML)
+    this.bar = 'I am Heb'
+    console.log(foo.innerHTML)
+    this.$nextTick(() => {
+      console.log(foo.innerHTML)
+    })
+    this.$nextTick(() => {
+      console.log('----- this is in nextTick ')
+    })
+    setTimeout(() => {
+      console.log('---- this is in setTimeout')
+    })
   }
 }
 </script>
