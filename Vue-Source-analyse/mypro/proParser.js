24345ae6bda9ac0e6c41b9f5a89a373f33f339f4
chao xu <979234374@qq.com>
2019-11-12 08:44:30 +0800

    add ssr

--- Vue-Source-analyse/mypro/proParser.js
+++ Vue-Source-analyse/mypro/proParser.js
@@ -2,7 +2,7 @@ const startSpaceReg = /^\s/g;
 const startReg = /^(?:(?:var|let|const)\s+|\s*)([a-zA-z_$][\w_$]*)\s*=\s*(?=new)/;
 const newPromiseReg = /^new\s+Promise\(\s*/;
 const bodyNewPromiseReg = new RegExp('\(\\s\+return\\s\+\)\?' + newPromiseReg.source.substring(1))
-const varableNameReg = /^[a-zA-z_$][\w_$]*/
+const varableNameReg = /^([a-zA-z_$][\w_$]*)\./
 const funcReg = /^function\s*([a-zA-z_$][\w_$]*)?\s*\((.*?)\)\s*|\((.*?)\)\s*(=>)\s*/
 const endFunPartReg = /^\s*\)/
 const thenReg = /^\s*\.then\(/
@@ -33,7 +33,7 @@ function parse (code) {
   }
   // if promise already exists
   else if (varableNameReg.test(code)) {
-    let matchedName = code.match(varableNameReg)[0]
+    let matchedName = code.match(varableNameReg)[1]
     advance(matchedName.length)
 
     if (results[matchedName]) {
@@ -79,23 +79,6 @@ function parse (code) {
   let handler = parseFuncDefine()
   if (handler) {
     activePromise.handler = handler
-    
-    if (handler.promises) {
-      handler.promises.forEach(promise) {
-        if (!promise.return) {
-          // custom promise
-          parse(promise.body)
-        } else {
-          // return promise
-          let childname = parse(promise.body)
-          activePromise.child[0] = results[childname]
-          // fast forward
-          while (activePromise.child.length) {
-            activePromise = activePromise.child[0]
-          }
-        }
-      }
-    }
     // end of the main part
     parseEndPart()
   }
@@ -143,29 +126,13 @@ function parse (code) {
           activePromise = {
             res: [ handlerRes ],
             rej: [ handlerRej ],
-            child: []
+            child: [],
+            siblings: []
           }
           prePromise.child.push(activePromise)
           // save number of then only when creating new promise
           if (isCreateNewP) {
             results[varName].then ++
-          }
-          // if then return promise
-          if (handlerRes.promises) {
-            handlerRes.promises.forEach(promise => {
-              if (!promise.return) {
-                // custom promise
-                parse(promise.body)
-              } else {
-                // return promise
-                let childname = parse(promise.body)
-                activePromise.child[0] = results[childname]
-                // fast forward
-                while (activePromise.child.length) {
-                  activePromise = activePromise.child[0]
-                }
-              }
-            })
           }
         } else {
           // multi then
@@ -244,13 +211,15 @@ function parse (code) {
   }
 
   function parsePromiseFromStartOffset (body, startIdx) {
-    let psBody = body.substr(startIdx) + '('
+    let psBody = '(' + body.substr(startIdx)
     let promiseBody = findCloseBrace(psBody, '(', ')')
     psBody = psBody.substr(promiseBody.length)
+    // pat it
+    promiseBody = 'new Promise' + promiseBody
     while (1) {
       let controlKeys = psBody.match(thenReg)
       if (controlKeys) {
-        psBody = psBody.substr(controlKeys[0]) + '('
+        psBody = '(' + psBody.substr(controlKeys[0].length)
         let thenBody = findCloseBrace(psBody, '(', ')')
         promiseBody += '.then' + thenBody
       } else {
@@ -271,7 +240,7 @@ function parse (code) {
   }
   
   function parseFuncBodyString () {
-    findCloseBrace(code, '{', '}')
+    return findCloseBrace(code, '{', '}')
   }
 
   function findCloseBrace (code, brace, antiBrace) {
@@ -302,6 +271,52 @@ function parse (code) {
   function advance (n) {
     index += n;
     code = code.substring(n);
+  }
+}
+
+function optimize () {
+  // in this step, we handle some new promises in then or return blocks
+  // just handle the initialized results in the first pass 
+  for (key in results) {
+    let result = results[key]
+    while (result) {
+      let next = result.child[0]
+      if (result.res) {
+        result.res.forEach(item => {
+          if (!item) return
+          item.promises.forEach(promise => {
+            let name = parse(promise.body)
+            if (!promise.return) {
+              result.siblings[0] = results[name]
+            } else {
+              result.child[0] = results[name]
+              let j = results[name].then, last = results[name]
+              while (j--) {
+                last = last.child[0]
+              }
+              last.child.push(next)
+            }
+          })
+        })
+        result.rej.forEach(item => {
+          if (!item) return
+          item.promises.forEach(promise => {
+            let name = parse(promise.body)
+            if (!promise.return) {
+              result.siblings[1] = results[name]
+            } else {
+              result.child[1] = results[name]
+              let j = results[name].then, last = results[name]
+              while (j--) {
+                last = last.child[0]
+              }
+              last.child.push(next)
+            }
+          })
+        })
+      }
+      result = result.child[0]
+    }
   }
 }
 
---
+++ ssr/components/detail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    This product is very useful for insomnia and obsession
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail'
+}
+</script>
