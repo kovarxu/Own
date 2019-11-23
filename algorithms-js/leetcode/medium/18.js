b17febbb8fcb325ec23dc590555ba8f8367ae53f
CX-SteelArm <979234374@qq.com>
2019-11-23 15:46:27 +0800

    modify parser

--- algorithms-js/leetcode/medium/18.js
+++ algorithms-js/leetcode/medium/18.js
@@ -3,4 +3,32 @@
 
 // Each number in candidates may only be used once in the combination.
 
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+  candidates.sort((a,b) => a-b);
+  let result = [];
+  let curSum = 0, cum = [];
+  search(0, curSum, target, cum, result);
 
+  function search(index, curSum, target, cum, result) {
+    if (curSum == target) {
+      result.push(cum.slice());
+      return true;
+    } else if (curSum < target && index < candidates.length) {
+      for (let j = index; j < candidates.length; j++) {
+        // get gid of repeat
+        if (j > index && candidates[j] == candidates[j-1]) continue;
+        let dum = cum.slice();
+        let next = candidates[j];
+        dum.push(next);
+        if (search(j+1, curSum + next, target, dum, result)) break;
+      }
+    }
+  }
+  
+  return result;
+};
--- tools/myPromise/fortest.js
+++ tools/myPromise/fortest.js
@@ -20,3 +20,34 @@ new Promise((resolve, reject) => {
     .then(() => {
       console.log("log: 外部第二个then");
     });
+
+
+new Promise((resolve, reject) => {
+  resolve(23);
+})
+.then(data => {
+  return data + 2;
+})
+.then(data => {
+  throw new Error('I am an error')
+})
+.then(data => {
+  return data;
+}, err => {
+  return err;
+})
+.catch(err => {
+  console.log(err);
+  return err.toString();
+})
+
+
+var p1 = new Promise((resolve, reject) => {
+  resolve(345);
+}).then((data) => {
+  return new Promise((resolve, reject) => {
+    resolve('abc');
+  })
+  .then(data => { console.log(data) })
+  .then(data => { console.log(data + 'def') })
+}).then(data => { console.log(data) + 125})
