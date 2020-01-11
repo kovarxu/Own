11ff4216de9763a0b81d09e4633a2551ab1ae220
CX-SteelArm <979234374@qq.com>
2020-01-11 23:23:55 +0800

    update all

---
+++ algorithms-js/leetcode/medium/01.5.js
@@ -0,0 +1,11 @@
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+// Given a string, find the length of the longest substring without repeating characters.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// sliding window && HashSet
+var lengthOfLongestSubstring = function(s) {
+  
+};
---
+++ algorithms/leet/common/3sum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+# https://leetcode-cn.com/problems/3sum/
+# double pointer
+
+class Solution:
+  def threeSum(self, nums: List[int]) -> List[List[int]]:
+    pass
+
+    
+
+
+if (__name__ == '__main__'):
+  pass
