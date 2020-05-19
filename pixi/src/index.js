c173df258a6c94c718af963bbdaeffa44ffafbee
chao xu <979234374@qq.com>
2020-05-19 11:04:46 +0800

    displacement

--- pixi/src/index.js
+++ pixi/src/index.js
@@ -35,6 +35,16 @@ const app = new PIXI.Application({
   height: window.innerHeight
 })
 
+const textureWater = PIXI.Texture.from('http://127.0.0.1:5000/assets/water.jpg')
+const water = new PIXI.Sprite(textureWater)
+
+water.x = app.renderer.width / 2
+water.y = app.renderer.height / 2
+water.anchor.x = 0.5
+water.anchor.y = 0.5
+app.stage.addChild(water)
+
+
 const texture = PIXI.Texture.from('http://127.0.0.1:5000/assets/eggHead.png')
 const img = new PIXI.Sprite(texture)
 
@@ -43,7 +53,10 @@ img.y = app.renderer.height / 2
 img.anchor.x = 0.5
 img.anchor.y = 0.5
 // img.filters = [ new PIXI.Filter(vshader, fshader, uniform) ]
-img.filters= [ new PIXIFILTERS.AsciiFilter(10) ]
+// img.filters= [ new PIXIFILTERS.AsciiFilter(10) ]
+img.filters = [ new PIXI.filters.DisplacementFilter(water, 16) ]
+console.log(img.filters)
+console.log(app.renderer.screen.width, app.renderer.screen.height)
 app.stage.addChild(img)
 app.ticker.add(animate)
 
